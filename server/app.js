f257285d4b093a4ea79ad73c875f486c596a11a0
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-12-12 16:47:06 -0600

    cron/nodemail function working but inside app.js

--- server/app.js
+++ server/app.js
@@ -40,32 +40,28 @@ app.listen(portDecision, function(){
 
 var transporter = nodemailer.createTransport();
 var cronJob = cron.job("0 */1 * * * *", function() {
-  console.log("Getting here");
-  app.get('/', function(req, res) {
-    console.log("Got inside get in cron job");
-    User.find({},
-      function(err, collection) {
-        if(err) {
-          console.log('Error in GET: ', err);
-        } else {
-            for (var i = 0; i < collection.length; i++) {
-              var mailOptions = {
-                from: 'alex.hermundson@gmail.com',
-                to: collection[i].email,
-                subject: 'hello',
-                html: '<b>Hi there, ' + collection[i].first_name + '</b>.'
-              };
-              transporter.sendMail(mailOptions, function(error, info) {
-              if(error) {
-                console.log("Error: ", error);
-              } else {
-                console.log("Message Sent: ", info.response);
-              }
-              });
+  User.find({},
+    function(err, collection) {
+      if(err) {
+        console.log('Error in GET: ', err);
+      } else {
+          for (var i = 0; i < collection.length; i++) {
+            var mailOptions = {
+              from: 'alex.hermundson@gmail.com',
+              to: collection[i].email,
+              subject: 'hello',
+              html: '<b>Hi there, ' + collection[i].first_name + '</b>.'
+            };
+            transporter.sendMail(mailOptions, function(error, info) {
+            if(error) {
+              console.log("Error: ", error);
+            } else {
+              console.log("Message Sent: ", info.response);
             }
-        }
+            });
+          }
       }
-    );
-  });
+    }
+  );
 });
 cronJob.start();
