64b8755b526a90226dcb121c80f582ed4fbb59f4
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-12-14 10:23:40 -0600

    added google popup into recommendBook Controller

--- server/public/scripts/controllers/recommendBook.controller.js
+++ server/public/scripts/controllers/recommendBook.controller.js
@@ -54,15 +54,16 @@ myApp.controller('RecommendBookController', ['BookFactory', 'UserFactory', '$uib
   self.previewBook = function(index) {
     self.isbn = self.books[index].volumeInfo.industryIdentifiers[0].identifier;
     console.log("ISBN: ", self.isbn);
-    var modalInstance = $uibModal.open({
-      templateUrl: './views/templates/preview-book-modal.html',
-      controller: 'PreviewModalController',
-      controllerAs: 'pm'
-    });
-    modalInstance.result.then(function(response) {
-      console.log("modal result: ", response);
-      getBooks();
-    });
+    GBS_insertPreviewButtonPopup('ISBN:' + self.isbn);
+    // var modalInstance = $uibModal.open({
+    //   templateUrl: './views/templates/preview-book-modal.html',
+    //   controller: 'PreviewModalController',
+    //   controllerAs: 'pm'
+    // });
+    // modalInstance.result.then(function(response) {
+    //   console.log("modal result: ", response);
+    //   getBooks();
+    // });
   }
 
   self.recommend();
